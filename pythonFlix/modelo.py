71e2ecbfc230f227f076f453feedeb0091773594
PolegolasXD <polegolasplays@gmail.com>
2023-04-07 12:19:46 -0300

    feat: criando property - protegendo os dados da aplicação com "private -adicionando property para falicitar a manutenção

--- pythonFlix/modelo.py
+++ pythonFlix/modelo.py
@@ -1,19 +1,58 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
 
-avengers = Filme('vingadore guerra infinita', 2018, 160)
-print(avengers.nome)
+    @property
+    def likes(self):
+        return self.__likes
 
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+avengers = Filme('\n- vingadore guerra infinita', 2018, 160)
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    def dar_like(self):
+        self.__likes += 1
+
+        
 
 theLastOfUs = Serie('the last of us', 2023, 160)
-print(f'\n- Nome: {theLastOfUs.nome} \n- Ano: {theLastOfUs.ano} \n- Temporadas: {theLastOfUs.temporadas}')
+
+avengers.dar_like()
+theLastOfUs.dar_like()
+
+
+print(f'\n- Nome: {avengers.nome} \n- Ano: {avengers.ano} \n- Temporadas: {avengers.duracao} \n- Likes: {avengers.likes}')
+print(f'\n- Nome: {theLastOfUs.nome} \n- Ano: {theLastOfUs.ano} \n- Temporadas: {theLastOfUs.temporadas} \n- Likes: {theLastOfUs.likes}')
