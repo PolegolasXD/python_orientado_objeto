01753b1931b455ab1879943c79a348a593af2b6f
PolegolasXD <polegolasplays@gmail.com>
2023-04-07 12:53:42 -0300

    feat: otimizando codigo -tirando as repetiões dos codigos, e adicionando a uma classe mãe -utilizando herança e superClass para atribuir os objetos e chamalos

--- pythonFlix/modelo.py
+++ pythonFlix/modelo.py
@@ -1,58 +1,42 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
-avengers = Filme('\n- vingadore guerra infinita', 2018, 160)
-
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    def dar_like(self):
-        self.__likes += 1
 
         
-
+avengers = Filme('\n- vingadore guerra infinita', 2018, 160)
 theLastOfUs = Serie('the last of us', 2023, 160)
 
 avengers.dar_like()
 theLastOfUs.dar_like()
-
 
 print(f'\n- Nome: {avengers.nome} \n- Ano: {avengers.ano} \n- Temporadas: {avengers.duracao} \n- Likes: {avengers.likes}')
 print(f'\n- Nome: {theLastOfUs.nome} \n- Ano: {theLastOfUs.ano} \n- Temporadas: {theLastOfUs.temporadas} \n- Likes: {theLastOfUs.likes}')
